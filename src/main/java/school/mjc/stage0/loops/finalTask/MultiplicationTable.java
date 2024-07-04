8ce36a89c3ad8fb4404cf8969c20ed0091fa49b1
hkorrizzz <tutvse253@gmail.com>
2024-07-04 21:51:02 +0300

    ttt

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.finalTask;
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){ //i*1=i i*2=2i
         for (int i = 1; i <= 10; i++) {
-            System.out.printf(i + " x " + numberTableToPrint + " = " + i*numberTableToPrint + " ");
+            System.out.printf(i + " x " + numberTableToPrint + " = " + i*numberTableToPrint);
         }
     }
 }
